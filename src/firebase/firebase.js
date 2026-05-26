282c805f56b826b2de60f5949e1fb026801ff452
arijeet <arijeet@tentworks.io>
2026-05-26 23:25:50 +0530

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,24 +1,27 @@
 ﻿import { initializeApp } from "firebase/app";
 
-console.log(import.meta.env.FIREBASE_API_KEY)
+import _ from "underscore"
+
+let config = {};
+
+if (import.meta.env.DEV) {
+    try {
+        config = await import('../../en.local.json');
+        config = config.default;
+    } catch {
+        console.log('en.local.json not found');
+    }
+}
+
 // Replace these values with your Firebase project config
 // Firebase Console → Project Settings → Your apps → SDK setup
 const firebaseConfig = {
-
-    apiKey: "AIzaSyCr5HJx5OTS-wACLq-iuXzspeDw466QyUI",
-
-    authDomain: "forge-order-form.firebaseapp.com",
-
-    projectId: "forge-order-form",
-
-    storageBucket: "forge-order-form.firebasestorage.app",
-
-    messagingSenderId: "367321711778",
-
-    appId: "1:367321711778:web:b1acd7e02c8c15a08b03fb",
-
-    measurementId: "G-S56JMVNFZX"
-
+    apiKey:            _.isEmpty(import.meta.env.FIREBASE_API_KEY) ? config.apiKey : import.meta.env.FIREBASE_API_KEY,
+    authDomain:        _.isEmpty(import.meta.env.FIREBASE_AUTH_DOMAIN) ? config.authDomain : import.meta.env.FIREBASE_AUTH_DOMAIN,
+    projectId:         _.isEmpty(import.meta.env.FIREBASE_PROJECT_ID) ? config.projectId : import.meta.env.FIREBASE_PROJECT_ID,
+    storageBucket:     _.isEmpty(import.meta.env.FIREBASE_STORAGE_BUCKET) ? config.storageBucket : import.meta.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: _.isEmpty(import.meta.env.FIREBASE_MESSAGING_SENDER_ID) ? config.messagingSenderId : import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId:             _.isEmpty(import.meta.env.FIREBASE_APP_ID) ? config.appId : import.meta.env.FIREBASE_APP_ID,
 };
 
 const app            = initializeApp(firebaseConfig);
